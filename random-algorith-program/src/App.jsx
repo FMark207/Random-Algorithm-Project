af22d9c2edd5471e6fc05c5cb2193996246a72ab
Farkas Márk <farkas.mark@mechwart.local>
2024-10-10 11:35:20 +0200

    Reworked-Header

--- random-algorith-program/src/App.jsx
+++ random-algorith-program/src/App.jsx
@@ -1,40 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-import GistEmbed from './GistEmbed.jsx';
 import 'https://kit.fontawesome.com/3b31cb99f3.js'
 
+
 function randomNum() {
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 100);
 }
 
 function Header() {
-  return (
-    <div className="header">
-      <h1 className="header-title">Random Number Sorting</h1>
-      <h1 className="header-display-array">[{randomNum()},{randomNum()},{randomNum()},{randomNum()}]</h1>
-      <button className="interactive-button" onClick={() => {
-        let numArray = document.querySelector(".header-display-array");
-        numArray.innerText = `[${randomNum()},${randomNum()},${randomNum()},${randomNum()}]`;
-      }}>
-        Regenerate <i className="fa-solid fa-arrows-spin spinning-arrow"></i>
-      </button>
+  return(
+    <div className='header'>
+      <p className='header-text'>Start Learning Random Sorting Algorithms Here</p>
+      <h1 className='header-title'>Sorting Algorithms. <span className='grad-text'>Learn It Quick.</span></h1>
+      <button className='header-btn'>+ Get Started Now</button>
+      
     </div>
-  );
+  )
 }
 
 function Sorting() {
   return (
     <div className='sorting'>
       <div className='sorting-desc'>
-        <h1>Sorting Title</h1>
+        <h1 className='sorting-title'>Sorting Title</h1>
         <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est modi voluptate ratione eligendi, molestias at cumque voluptatem dolores! Nemo voluptate suscipit tempora, illum eos veniam. Quis optio labore est animi!</h2>
-      </div>
-      <div className='display-code'>
-        <GistEmbed gistId={"18e48f53c34098554cd339926cb2bc0f"}></GistEmbed>
       </div>
     </div>
   );
 }
+
+
 
 function App() {
   return (
