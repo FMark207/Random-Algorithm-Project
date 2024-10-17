2c55dbf2fa91326010e851633f921e7741fff47c
FMark207 <mark.farkas.207@gmail.com>
2024-10-17 08:04:46 +0200

    Made Paragraph Dynamic

--- random-algorith-program/src/App.jsx
+++ random-algorith-program/src/App.jsx
@@ -5,12 +5,16 @@ import Cards from './components/Cards_Comp/cards';
 import placeholder from "./components/Paragraph_Comp/placeholder.svg"
 import Tab from './components/Paragraph_Comp/Paragraph';
 
+
+
 function App() {
   return (
     <>
       <Header />
       <Cards />
-      <Tab source={placeholder}/>
+      <Tab source={placeholder} 
+      title="Sorting Algorithm" 
+      desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maxime ea? Soluta nesciunt, ut perferendis provident incidunt magni quia expedita suscipit qui, alias deleniti deserunt fuga illo minus doloremque dolorem!"/>
     </>
   );
 }
--- random-algorith-program/src/components/Paragraph_Comp/Paragraph.jsx
+++ random-algorith-program/src/components/Paragraph_Comp/Paragraph.jsx
@@ -5,8 +5,8 @@ export default function Paragraph(props) {
     return(
       <div className='paragraph'>
         <div className='paragraph-text'>
-            <h1>Sorting Title</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, error blanditiis officia vitae aliquam omnis adipisci sed laudantium saepe? Doloremque, possimus? Eveniet sed quam quia placeat, nulla velit quasi fugiat.</p>
+            <h1>{props.title}</h1>
+            <p>{props.desc}</p>
         </div>
         <img src={props.source} className="alg-display"/>
         
